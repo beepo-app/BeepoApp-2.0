c723a62f888c69d07871d416903df11d81db2784
Favour <hello.williamsfav@gmail.com>
2024-10-11 21:37:49 +0100

    initial commit

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,34 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:awesome_notifications/awesome_notifications_web.dart';
+import 'package:camera_web/camera_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:image_cropper_for_web/image_cropper_for_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:uni_links_web/uni_links_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AwesomeNotificationsWeb.registerWith(registrar);
+  CameraPlugin.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  ImageCropperPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UniLinksPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
