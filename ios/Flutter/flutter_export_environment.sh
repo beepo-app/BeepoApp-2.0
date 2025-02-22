85fa667096921c023439461af52d1024d73078eb
Favour williams <90847660+hello-favour@users.noreply.github.com>
2025-02-22 02:29:17 +0100

    initial build

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Favour Williams\Documents\projects\beepo\BeepoApp-2.00"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Favour Williams\Documents\New Beepo\BeepoApp-2.0"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
